287e5bb5afa20950dda40b19f821c154827edc7d
ShoneSingLone <ShoneSingLone@gmail.com>
2021-05-29 00:33:21 +0800

    fix(all): miss Music.vue

---
+++ packages/wiichat/src/views/Music.vue
@@ -0,0 +1,22 @@
+<template></template>
+<script>
+import { app as Music } from "@ventose/music-player/dist/music-player.es.js";
+import $ from "jquery";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    ((id) => {
+      const idSelector = `#${id}`;
+      if (!Music.isMounted) {
+        $("<div/>", { id }).appendTo($("body"));
+        Music.mount(idSelector);
+        Music.isMounted = true;
+      }
+    })("app-music-id");
+  },
+});
+</script>
+
+<style>
+</style>
