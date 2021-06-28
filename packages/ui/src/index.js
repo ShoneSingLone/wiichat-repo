769e16fd860544be32dff158c86efbc49f5fdb24
ShoneSingLone <ShoneSingLone@gmail.com>
2021-06-28 14:09:40 +0800

    remove admin

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -3,28 +3,28 @@ import xBtn from "./components/button/index";
 import xIcon from "./components/icon/index.vue";
 import xRipple, {installXRippleToBody} from "./components/Ripple/index.vue";
 import _color from "./utils/colors";
-import {setAliFont} from "./utils/font"
+import {setAliFont} from "./utils/font";
 import _ from "lodash";
 import "ant-design-vue/dist/antd.css";
 // import { Button, Card, ConfigProvider, Icon, Menu, Dropdown, } from "ant-design-vue";
 import antdv from "ant-design-vue";
-import "./styles/common.scss"
+import "./styles/common.scss";
 
 const componentList = [xBtn, xIcon];
 // const antdv_componets = [Button, Card, ConfigProvider, Icon, Menu, Dropdown];
 
 /* export  */
-export {windowOnResize} from "./utils/resize"
+export {windowOnResize} from "./utils/resize";
 export const color = _color;
 export default {
-    antdv,
-    install: (app, {mount}) => {
-        setAliFont();
-        /* 仿material design 水波纹 */
-        installXRippleToBody(xRipple,mount);
-        /* 注册app全局组件 */
-        app.use(antdv)
-        // antdv_componets.forEach(component => app.use(component));
-        componentList.forEach(component => app.component(component.name, component));
-    }
+	antdv,
+	install: (app, {mount}) => {
+		setAliFont();
+		/* 仿material design 水波纹 */
+		installXRippleToBody(xRipple,mount);
+		/* 注册app全局组件 */
+		app.use(antdv);
+		// antdv_componets.forEach(component => app.use(component));
+		componentList.forEach(component => app.component(component.name, component));
+	}
 };
--- packages/ui/src/main.js
+++ packages/ui/src/main.js
@@ -1,13 +1,13 @@
 import {
-    createApp
-} from 'vue'
-import AppUI from './AppUI.vue'
+	createApp
+} from "vue";
+import AppUI from "./AppUI.vue";
 import myui from "./index";
 import {router} from "./router.js";
 
 createApp(AppUI)
-    .use(router)
-    .use(myui, {
-        mount: "#app"
-    })
-    .mount('#app')
+	.use(router)
+	.use(myui, {
+		mount: "#app"
+	})
+	.mount("#app");
